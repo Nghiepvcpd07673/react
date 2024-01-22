c0c0aef05be919d9797d2fc455f89db7747a0b5e
Nghiepvcpd07673 <nghiepvcpd07673@fpt.edu.vn>
2024-01-22 17:11:25 +0700

    add

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MyLab3 from './src/screens/lab3'
+import MyLab3_1 from './src/screens/lab3/lab3_3'
+import MyAssignment_1 from './src/screens/assignment'
+import Login from './src/screens/assignment/login'
+import Register from './src/screens/assignment/register'
+import Home from './src/screens/assignment/home'
+import MyLab4 from './src/screens/lab4'
+const App = () => {
+  return (
+    <MyAssignment_1/>
+  )
+}
+
+export default App
